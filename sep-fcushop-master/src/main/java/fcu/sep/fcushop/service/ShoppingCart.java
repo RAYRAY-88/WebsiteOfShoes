33ff6789c506d158dc82eeacb5e39d57bb58b838
D0816015 <yupinhsien@gmail.com>
2022-01-09 00:37:23 +0800

    購物車完成(排版、內容會再更新)

--- sep-fcushop-master/src/main/java/fcu/sep/fcushop/service/ShoppingCart.java
+++ sep-fcushop-master/src/main/java/fcu/sep/fcushop/service/ShoppingCart.java
@@ -26,4 +26,15 @@ public class ShoppingCart {
             return connection.createQuery(query).executeAndFetch(Product.class);
         }
     }
+
+    public List<Product> getProducts(String keyword) {
+        try (Connection connection = sql2oDbHandler.getConnector().open()) {
+            String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
+                    + " from product where name like :keyword";
+
+            return connection.createQuery(query)
+                    .addParameter("keyword", "%"+keyword+"%")
+                    .executeAndFetch(Product.class);
+        }
+    }
 }
