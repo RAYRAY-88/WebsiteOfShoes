592072c00c58793e28ee8e9efb192bc673a2b707
Ray <ray8899810@gmail.com>
2022-01-10 13:56:14 +0800

    登入註冊部分alert()沒有加會噴錯

--- sep-fcushop-master/src/main/java/fcu/sep/fcushop/model/Account.java
+++ sep-fcushop-master/src/main/java/fcu/sep/fcushop/model/Account.java
@@ -2,7 +2,7 @@ package fcu.sep.fcushop.model;
 
 public class Account {
 
-  private int user_id = 1 ;
+  private int user_id = 1;
   private String username;
   private String password;
 
@@ -11,25 +11,13 @@ public class Account {
     this.username = username;
     this.password = password;
   }
+  public Account(){}
+  public int getID(){return user_id;}
+  public void setID(int ID){this.user_id=ID;}
 
-  public Account(){
+  public String getUName() {return username;}
+  public void setUName(String name){this.username = name;}
 
-  }
-  public int getID() {
-    return user_id;
-  }
-  public void setID(int ID) {
-    this.user_id = ID;
-  }
-
-  public String getUName() {
-    return username;
-  }
-  public void setUName(String name) {
-    this.username = name;
-  }
-
-  public String getPassword() {return password;}
-  public void setPassword(String password) {this.password = password;}
-
+  public String getPassword(){return password;}
+  public void setPassword(String password){this.password = password;}
 }
